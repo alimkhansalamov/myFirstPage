3496fab6eb4eec0811884d13366a89dfb9a2ca28
Alimkhan8195 <alimkhan.salamov@gmail.com>
2022-11-01 02:00:09 +0100

    Possibility to search for a todo

--- src/js/details.js
+++ src/js/details.js
@@ -2,6 +2,7 @@
 // with get method, we are getting only id number and saving it in id variable
 const id = new URLSearchParams(window.location.search).get('id');
 const container = document.querySelector('.details');
+const deleteBtn = document.querySelector('.delete');
 
 const renderDetails = async () => {
   const res = await fetch('http://localhost:3000/todos/' + id);
@@ -15,5 +16,14 @@ const renderDetails = async () => {
 
   container.innerHTML = template;
 };
+
+deleteBtn.addEventListener('click', async (e) => {
+  const res = await fetch('http://localhost:3000/todos/' + id, {
+    method: 'DELETE',
+  });
+
+  window.location.replace('/src/index.html');
+});
+const deleteTodo = async () => {};
 
 window.addEventListener('DOMContentLoaded', () => renderDetails());
